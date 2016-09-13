cf6886b56bfed1870c19c8f320d4fad66e805c09
mauricioFesc <mauricio_fesc@hotmail.com>
2016-09-13 14:32:29 -0500

    if-else  if
    usando operadores lógicos y variable del tipo char

---
+++ NotaDeMateria.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  char nota; //una variable del tipo char puede almacenar una letra, símbolo o dígito, SOLO UNO
+  printf("\n Programa calificacion de materia ");
+  printf("\n Ingrese una nota (caracter) \t");
+  scanf("%c",&nota);
+  if(nota=='a'||nota=='A') //la variable del tipo char se compara encerrándola entre ''
+  {                        /*  Si no se hiciera eso, se tomaría el caracter a o A como otra variable en el caso de este programa*/
+    printf("\n Sobresaliente ");
+  }
+  else
+  {
+    printf("\n Debes mejorar ");
+  }
+  system("PAUSE");
+  return 0;
+}
